ed9c8e641a581791c6457949ea98a4ebe1ae533c
IvanGer212 <Comradeivan88@gmail.com>
2022-09-18 23:13:13 +0300

    Start project

---
+++ src/main/java/com/geekbrains/ru/springlesson2_hw/SpringLesson2HwApplication.java
@@ -0,0 +1,12 @@
+package com.geekbrains.ru.springlesson2_hw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLesson2HwApplication {
+
+    public static void main(String[] args){
+    }
+
+}
